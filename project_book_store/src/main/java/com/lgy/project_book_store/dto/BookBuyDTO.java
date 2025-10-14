7dbc1693d69eb0c5173139c32b4c24124466c8c6
swj6498 <119040020+swj6498@users.noreply.github.com>
2025-10-14 16:04:26 +0900

    Add files via upload

--- project_book_store/src/main/java/com/lgy/project_book_store/dto/BookBuyDTO.java
+++ project_book_store/src/main/java/com/lgy/project_book_store/dto/BookBuyDTO.java
@@ -13,5 +13,8 @@ public class BookBuyDTO {
     private int buy_id;
     private int book_id;
     private String user_id;
+    private int quantity;
     private Date purchase_date;
+    
+    private BookDTO book;
 }
