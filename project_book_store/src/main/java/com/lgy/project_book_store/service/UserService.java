4ac1eac095186151da32f197efadb6b810479a38
NamTaeYoung <150254921+NamTaeYoung@users.noreply.github.com>
2025-10-15 09:12:28 +0900

    Update UserService.java

--- project_book_store/src/main/java/com/lgy/project_book_store/service/UserService.java
+++ project_book_store/src/main/java/com/lgy/project_book_store/service/UserService.java
@@ -8,4 +8,5 @@ import com.lgy.project_book_store.dto.UserDTO;
 public interface UserService {
 	public ArrayList<UserDTO> loginYn(HashMap<String, String> param);
 	public void register(HashMap<String, String> param);
+	public int checkId(String id);
 }
