0d36f2c632086e9c66a96b00837af030ad960df1
jseulmi <127377332+jseulmi@users.noreply.github.com>
2025-10-15 09:49:47 +0900

    Update SearchDTO.java

--- project_book_store/src/main/java/com/lgy/project_book_store/dto/SearchDTO.java
+++ project_book_store/src/main/java/com/lgy/project_book_store/dto/SearchDTO.java
@@ -4,16 +4,16 @@ import lombok.Data;
 
 @Data
 public class SearchDTO {
-    private Integer bookId;
-    private String  bookTitle;
-    private String  bookWriter;
-    private String  bookPub;
-    private java.sql.Date  bookDate;     
-    private Integer genreId;
-    private String  genreName;
-    private Integer bookPrice;
-    private Integer bookCount;
-    private String  bookComm;
-    private String  bookIsbn;
-    private String  bookImagePath; 
+    private Integer book_id;
+    private String  book_title;
+    private String  book_writer;
+    private String  book_pub;
+    private java.sql.Date  book_date;     
+    private Integer genre_id;
+    private String  genre_name;
+    private Integer book_price;
+    private Integer book_count;
+    private String  book_comm;
+    private String  book_isbn;
+    private String  book_image_path; 
 }
