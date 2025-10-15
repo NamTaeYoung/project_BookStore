a018bbc84f4683bf44ccfa3f5131cf97133fae85
NamTaeYoung <150254921+NamTaeYoung@users.noreply.github.com>
2025-10-15 16:29:32 +0900

    Update UserServicelmpl.java

--- project_book_store/src/main/java/com/lgy/project_book_store/service/UserServicelmpl.java
+++ project_book_store/src/main/java/com/lgy/project_book_store/service/UserServicelmpl.java
@@ -1,37 +1,59 @@
 package com.lgy.project_book_store.service;
 
-import java.util.ArrayList;
+import java.util.Map;
 import java.util.HashMap;
 
-import org.apache.ibatis.session.SqlSession;
+import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.lgy.project_book_store.dao.UserDAO;
-import com.lgy.project_book_store.dto.UserDTO;
-
 @Service
-public class UserServicelmpl implements UserService{
+public class UserServicelmpl implements UserService {
 
-	@Autowired
-	private SqlSession sqlSession;
-	
-	@Override
-	public ArrayList<UserDTO> loginYn(HashMap<String, String> param) {
-		UserDAO dao = sqlSession.getMapper(UserDAO.class);	
-		ArrayList<UserDTO> list = dao.loginYn(param);
-		return list;
-	}
+    @Autowired
+    private SqlSessionTemplate sqlSession;
 
-	@Override
-	public void register(HashMap<String, String> param) {
-		UserDAO dao = sqlSession.getMapper(UserDAO.class);
-		dao.register(param);
-	}
+    /** 로그인 검증: users.user_pw를 읽어와 비교 (현 상태 유지) */
+    public boolean loginYn(Map<String, String> param) {
+        // mapper: loginYn -> 비밀번호만 조회
+        String dbPw = (String) sqlSession.selectOne(
+            "com.lgy.project_book_store.dao.UserDAO.loginYn", param);
+        if (dbPw == null) return false;
+        String inputPw = param.get("user_pw");
+        return dbPw.equals(inputPw);
+    }
 
-	@Override
-	public int checkId(String id) {
-		UserDAO dao = sqlSession.getMapper(UserDAO.class);
-        return dao.checkId(id);
-	}
+    /** 마이페이지 조회 */
+    public Map<String, Object> getUser(String userId) {
+        Map<String, Object> m = new HashMap<String, Object>();
+        m.put("user_id", userId);
+        return sqlSession.selectOne(
+            "com.lgy.project_book_store.dao.UserDAO.selectUser", m);
+    }
+
+    /** 마이페이지 수정 */
+    public int updateUser(Map<String, String> param) {
+        return sqlSession.update(
+            "com.lgy.project_book_store.dao.UserDAO.updateUser", param);
+    }
+    
+    /** 회원가입 로직 */
+    public int register(Map<String, String> param) {
+        // User.xml 의 <insert id="register"> 문 실행
+        return sqlSession.insert(
+            "com.lgy.project_book_store.dao.UserDAO.register", param);
+    }
+    
+    public int withdraw(Map<String, String> param) {
+        // 비번 일치하면 1, 아니면 0
+        return sqlSession.delete(
+            "com.lgy.project_book_store.dao.UserDAO.withdraw", param);
+    }
+
+    //아이디 중복 체크
+    @Override
+    public int checkId(String id) {
+        return sqlSession.selectOne("com.lgy.project_book_store.dao.UserDAO.checkId", id);
+    }
+
 }
