56b26bf72e33ad0ccead4be6e3fbb003a84ba3dd
NamTaeYoung <150254921+NamTaeYoung@users.noreply.github.com>
2025-10-15 16:29:15 +0900

    Update UserService.java

--- project_book_store/src/main/java/com/lgy/project_book_store/service/UserService.java
+++ project_book_store/src/main/java/com/lgy/project_book_store/service/UserService.java
@@ -1,12 +1,24 @@
 package com.lgy.project_book_store.service;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-
-import com.lgy.project_book_store.dto.UserDTO;
+import java.util.Map;
 
 public interface UserService {
-	public ArrayList<UserDTO> loginYn(HashMap<String, String> param);
-	public void register(HashMap<String, String> param);
-	public int checkId(String id);
+
+	/** 로그인 검증 */
+    boolean loginYn(Map<String, String> param);
+
+    /** 회원가입 */
+    int register(Map<String, String> param);
+
+    /** 마이페이지 조회 */
+    Map<String, Object> getUser(String userId);
+
+    /** 마이페이지 수정 */
+    int updateUser(Map<String, String> param);
+
+    /** 회원 탈퇴 */
+    int withdraw(Map<String, String> param);
+
+    /** 아이디 중복 체크 */
+    int checkId(String id);
 }
