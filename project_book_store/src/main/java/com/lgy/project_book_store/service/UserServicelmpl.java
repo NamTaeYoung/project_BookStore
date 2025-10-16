d6dc3224e0efd409ca5abf11c6d846237a336c56
NamTaeYoung <150254921+NamTaeYoung@users.noreply.github.com>
2025-10-16 11:35:00 +0900

    Update UserServicelmpl.java

--- project_book_store/src/main/java/com/lgy/project_book_store/service/UserServicelmpl.java
+++ project_book_store/src/main/java/com/lgy/project_book_store/service/UserServicelmpl.java
@@ -7,6 +7,8 @@ import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.lgy.project_book_store.dto.UserDTO;
+
 @Service
 public class UserServicelmpl implements UserService {
 
@@ -77,4 +79,5 @@ public class UserServicelmpl implements UserService {
 				"com.lgy.project_book_store.dao.UserDAO.resetLoginFail", user_id);
 	}
 }
+
 
