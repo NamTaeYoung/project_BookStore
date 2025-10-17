0e18e9a6062bd34e77e2f78877bb2407afe1a895
NamTaeYoung <150254921+NamTaeYoung@users.noreply.github.com>
2025-10-17 22:59:56 +0900

    Delete src directory

--- src/main/java/com/lgy/project_book_store/service/ProjectService.java
+++
@@ -1,7 +0,0 @@
-package com.lgy.project_book_store.service;
-
-import org.springframework.ui.Model;
-
-public interface ProjectService {
-	public int execute(Model model);
-}
--- src/main/java/com/lgy/project_book_store/service/SocialLoginService.java
+++
@@ -1,8 +0,0 @@
-package com.lgy.project_book_store.service;
-
-import com.lgy.project_book_store.dto.SocialUserDTO;
-
-public interface SocialLoginService {
-	public String getAccessToken(String code);
-	public SocialUserDTO getUserInfo(String accessToken);
-}
